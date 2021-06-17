ae30f1c18801e27cefb1c32e47d89fc54e03d64a
berabjesus <64360974+Berabjesus@users.noreply.github.com>
2021-06-17 10:11:02 +0300

    ADD resource folder

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -6,11 +6,13 @@ import gui.Window;
 public class Game {
     private Window window;
     private GameGui gameGui;
+    private int[][] fields;
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
     public  Game(){
         window = new Window("Tic Tac Toe", WIDTH, HEIGHT);
         gameGui = new GameGui();
+        fields = new int[3][3];
         window.add(gameGui);
     }
 }
